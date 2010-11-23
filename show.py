621623c4849f6b2d4b40a200e73d1e176679da70
Marcio Faustino <marciof@home>
2010-11-23 23:59:22 +0100

    - Major clean up. - Guess input syntax even when already colored to use an appropriate pager.

--- show.py
+++ show.py
@@ -2,20 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
+# TODO: Handle the KeyboardInterrupt exception gracefully.
 # TODO: Use a percentage of the current terminal height instead of a fixed
 #       number of lines.
-# TODO: Clean up (abstract logic behind classes, etc).
-# TODO: Handle the KeyboardInterrupt exception gracefully.
-# TODO: Detect missing programs and provide automatic installation or fallbacks.
-# TODO: Guess input syntax even if already colored to use an appropriate pager.
-# TODO: Pass real files to Kompare instead of diff output?
-# TODO: Allow override of the default diff pager program (e.g. opendiff kdiff3
-#       tkdiff xxdiff meld kompare gvimdiff diffuse ecmerge p4merge araxis
-#       emerge vimdiff).
+# TODO: Detect missing programs and provide automatic installation or fallbacks
+#       (e.g. opendiff kdiff3 tkdiff xxdiff meld kompare gvimdiff diffuse
+#       ecmerge p4merge araxis emerge vimdiff).
 
 
 # Standard library:
-import codecs, locale, re, subprocess, sys
+import abc, codecs, locale, re, subprocess, sys
 
 try:
     import argparse
@@ -44,21 +40,10 @@ class Arguments (argparse.ArgumentParser):
             return number
         
         arguments = [
-            ('-l', {
-                'dest': 'lines',
-                'default': 15,
-                'type': natural,
-                'help': 'Number of lines to display inline before paging.',
-            }),
             ('-L', {
                 'dest': 'label',
                 'action': 'append',
                 'help': '(diff)',
-            }),
-            ('-p', {
-                'dest': 'pager',
-                'action': 'append',
-                'help': 'Custom pager program to use and arguments.',
             }),
             ('-u', {
                 'action': 'store_true',
@@ -134,6 +119,24 @@ class Arguments (argparse.ArgumentParser):
 
 
 class Pager (object):
+    __metaclass__ = abc.ABCMeta
+    
+    
+    def accepts_color(self):
+        return True
+    
+    
+    @abc.abstractmethod
+    def close(self):
+        pass
+    
+    
+    @abc.abstractmethod
+    def write(self, text):
+        pass
+
+
+class StreamPager (Pager):
     def __init__(self, stream):
         self._stream = codecs.getwriter(locale.getpreferredencoding())(stream)
     
@@ -142,11 +145,11 @@ class Pager (object):
         self._stream.close()
     
     
-    def write(self, string):
-        self._stream.write(string)
+    def write(self, text):
+        self._stream.write(text)
 
 
-class ProgramPager (Pager):
+class ProgramPager (StreamPager):
     def __init__(self, command):
         self._process = subprocess.Popen(command, stdin = subprocess.PIPE)
         super(ProgramPager, self).__init__(self._process.stdin)
@@ -158,61 +161,105 @@ class ProgramPager (Pager):
         self._process.wait()
 
 
-def display(stream, text, lexer, formatter):
-    if lexer is not None:
-        text = pygments.highlight(text, lexer, formatter)
+class DiffPager (ProgramPager):
+    def __init__(self):
+        super(DiffPager, self).__init__(['kompare', '-o', '-'])
     
-    stream.write(text)
+    
+    def accepts_color(self):
+        return False
 
 
-def guess_lexer(file_name, text):
-    # Detect ANSI "color" escape sequences.
-    if re.search(r'\x1B\[\d+(;\d+)*m', text):
-        return None
+class TextPager (ProgramPager):
+    def __init__(self, tab_size = 4):
+        super(TextPager, self).__init__([
+            'less',
+            '--clear-screen',
+            '--RAW-CONTROL-CHARS',
+            '--tabs=%d' % tab_size,
+        ])
+
+
+class AutomaticPager (Pager):
+    def __init__(self, source_name, max_inline_lines, diff_mode):
+        self._source_name = source_name
+        self._max_inline_lines = max_inline_lines
+        self._diff_mode = diff_mode
+        
+        self._output = None
+        self._inline_buffer = ''
+        self._inline_lines = 0
+        
+        self._line_separator = '\n'
+        self._ansi_color_escape = r'\x1B\[\d+(;\d+)*m'
     
-    try:
-        lexer = pygments.lexers.guess_lexer_for_filename(file_name, text)
-    except pygments.util.ClassNotFound:
-        lexer = pygments.lexers.guess_lexer(text)
     
-    lexer.add_filter('codetagify')
-    return lexer
+    def close(self):
+        if self._output is None:
+            self._setup_output(self._inline_buffer)
+            self._display(self._inline_buffer)
+        
+        self._output.close()
+    
+    
+    def write(self, text):
+        text = re.sub(self._ansi_color_escape, '', text)
+        
+        if self._output is None:
+            self._inline_buffer += text
+            self._inline_lines += text.count(self._line_separator)
+            
+            if self._inline_lines <= self._max_inline_lines:
+                return
+            
+            text = self._inline_buffer
+            self._setup_output(text)
+        
+        self._display(text)
+    
+    
+    def _display(self, text):
+        self._output.write(pygments.highlight(text,
+            self._lexer, self._formatter))
+    
+    
+    def _guess_lexer(self, text):
+        if self._diff_mode:
+            lexer = pygments.lexers.DiffLexer()
+        else:
+            try:
+                lexer = pygments.lexers.guess_lexer_for_filename(
+                    self._source_name, text)
+            except pygments.util.ClassNotFound:
+                lexer = pygments.lexers.guess_lexer(text)
+        
+        lexer.add_filter('codetagify')
+        return lexer
+    
+    
+    def _setup_output(self, text):
+        lexer = self._guess_lexer(text)
+        
+        if self._inline_lines <= self._max_inline_lines:
+            self._output = StreamPager(sys.stdout)
+        elif self._diff_mode or isinstance(lexer, pygments.lexers.DiffLexer):
+            self._output = DiffPager()
+        else:
+            self._output = TextPager()
+        
+        if self._output.accepts_color():
+            self._lexer = lexer
+            self._formatter = pygments.formatters.Terminal256Formatter()
+        else:
+            self._lexer = pygments.lexers.TextLexer()
+            self._formatter = pygments.formatters.NullFormatter()
 
 
 args = Arguments().parse_args()
-formatter = pygments.formatters.Terminal256Formatter()
-lexer = pygments.lexers.DiffLexer() if args.diff_mode else None
-pager = None
-lines = []
+pager = AutomaticPager(args.file.name, 15, args.diff_mode)
 
 for line in args.file:
-    if pager is not None:
-        display(pager, line, lexer, formatter)
-        continue
-    
-    lines.append(line)
-    
-    if len(lines) >= args.lines:
-        text = ''.join(lines)
-        
-        if lexer is None:
-            lexer = guess_lexer(args.file.name, text)
-        
-        if args.pager is None:
-            if isinstance(lexer, pygments.lexers.DiffLexer):
-                args.pager = ['kompare', '-o', '-']
-                lexer = None
-            else:
-                args.pager = ['less', '-cRx4']
-        
-        pager = ProgramPager(args.pager)
-        display(pager, text, lexer, formatter)
-
-if pager is not None:
-    pager.close()
-elif len(lines) > 0:
-    text = ''.join(lines)
-    lexer = guess_lexer(args.file.name, text)
-    display(Pager(sys.stdout), text, lexer, formatter)
+    pager.write(line)
 
 args.file.close()
+pager.close()
