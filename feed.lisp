1d86c99aca379ef6c3601b13b531c6425fe4757f
Marcio Faustino <marciof@home>
2011-02-21 20:23:25 +0100

    Added RSS feed filtering for HD-Trailers.

--- feed.lisp
+++ feed.lisp
@@ -6,19 +6,40 @@
 
 (import 'metabang-bind:bind)
 
+
+(defun filter-feed-items (url predicate)
+  (bind (((status headers data) (trivial-http:http-get url))
+         (feed (xmls:parse data))
+         (channel (nth 2 feed)))
+    
+    (delete-if
+      (lambda (item)
+        (and (equalp (car item) "item")
+             (not (funcall predicate item))))
+      (cddr channel))
+    
+    (xmls:write-xml feed *standard-output*)))
+
+
+(defun hd-trailer-item? (item)
+  (let ((title (find-if (lambda (element) (equalp (car element) "title"))
+                        (cddr item))))
+    (cl-ppcre:scan "(?ix) \\( [^(]* (teaser | trailer) [^)]* \\)"
+                   (nth 2 title))))
+
+
 (defun ign-daily-fix-item? (item)
   (let ((title (find-if (lambda (element) (equalp (car element) "title"))
                         (cddr item))))
     (cl-ppcre:scan "(?ix) ign \\s+ daily \\s+ fix"
                    (nth 2 title))))
 
-(bind (((status headers feed-data)
-        (trivial-http:http-get "http://feeds.ign.com/ignfeeds/podcasts/games/"))
-       (feed (xmls:parse feed-data))
-       (channel (nth 2 feed)))
-  (delete-if
-    (lambda (item)
-      (and (equalp (car item) "item")
-           (not (ign-daily-fix-item? item))))
-    (cddr channel))
-  (xmls:write-xml feed *standard-output*))
+
+(filter-feed-items
+  "http://feeds.hd-trailers.net/hd-trailers/blog"
+  #'hd-trailer-item?)
+
+
+;; (filter-feed-items
+;;   "http://feeds.ign.com/ignfeeds/podcasts/games/"
+;;   #'ign-daily-fix-item?)
