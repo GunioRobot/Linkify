8734f7d528c68ca6e7a3d5a44d64e61ed3133a11
Marcio Faustino <marciof@home>
2011-05-08 10:38:58 +0200

    Handle network issues when loading an RSS feed.

--- feed.pl
+++ feed.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 use defaults;
+use HTTP::Status ();
 use List::MoreUtils qw(indexes);
 use Mojolicious::Lite;
 use XML::FeedPP ();
@@ -8,35 +9,46 @@ use XML::FeedPP ();
 
 # TODO: Keep favicon.ico from original feed.
 
-sub filter_feed_items(&$) {
-    my ($keep_item, $url) = @ARG;
-    my $feed = XML::FeedPP->new($url);
-    my @items_to_remove = indexes {!$keep_item->($ARG)} $feed->get_item;
+app->helper(filter_feed_items => sub {
+    my ($self, $url, $keep_item) = @ARG;
+    my $feed = try {
+        XML::FeedPP->new($url);
+    }
+    catch {
+        die $ARG unless $ARG =~ m/^\QLoading failed: $url\E/;
+        undef;
+    };
     
-    $feed->remove_item($ARG) for reverse @items_to_remove;
-    return $feed->to_string;
-}
+    if (defined $feed) {
+        my @items_to_remove = indexes {!$keep_item->($ARG)} $feed->get_item;
+        $feed->remove_item($ARG) for reverse @items_to_remove;
+        
+        $self->render(
+            format => 'rss',
+            text => $feed->to_string);
+    }
+    else {
+        $self->render(
+            format => 'txt',
+            status => HTTP::Status::HTTP_GATEWAY_TIMEOUT,
+            text => '');
+    }
+});
 
 
 # TODO: Include posters as inline attachments.
 
 get '/hd-trailers' => sub {
-    shift->render(
-        format => 'rss',
-        text => filter_feed_items
-            {$ARG->title =~ m/\( [^(]* (teaser | trailer) [^)]* \)/ix}
-            'http://feeds.hd-trailers.net/hd-trailers/blog',
-    );
+    shift->filter_feed_items(
+        'http://feeds.hd-trailers.net/hd-trailers/blog',
+        sub {$ARG->title =~ m/\( [^(]* (teaser | trailer) [^)]* \)/ix});
 };
 
 
 get '/ign-daily-fix' => sub {
-    shift->render(
-        format => 'rss',
-        text => filter_feed_items
-            {$ARG->title =~ m/\b IGN \s+ Daily \s+ Fix \b/ix}
-            'http://feeds.ign.com/ignfeeds/podcasts/games/',
-    );
+    shift->filter_feed_items(
+        'http://feeds.ign.com/ignfeeds/podcasts/games/',
+        sub {$ARG->title =~ m/\b IGN \s+ Daily \s+ Fix \b/ix});
 };
 
 
