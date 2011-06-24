ac033661bbe860ad37b58a8ce4618c66eff377e2
Marcio Faustino <marciof@home>
2011-06-24 20:04:28 +0200

    Added initial backup task for Winamp.

---
+++ automate/backup.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+
+# Standard library:
+from __future__ import division, print_function, unicode_literals
+import bz2, tarfile
+
+# External modules:
+from defaults import *
+
+# Internal modules:
+import automate.task, automate.util
+
+
+class BackupFile (tarfile.TarFile):
+    def __init__(self, name):
+        tarfile.TarFile.__init__(self,
+            fileobj = bz2.BZ2File(name + '.tar.bz2', mode = 'w'),
+            mode = 'w')
+        
+        self._extfileobj = False
+    
+    
+    def __enter__(self):
+        return self
+    
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+    
+    
+    def add_files(self, folder, files):
+        for file in files:
+            self.add(folder.child(file), file)
+
+
+class BackupTask (automate.task.PeriodicTask):
+    __metaclass__ = ABCMeta
+    
+    
+    @abstractproperty
+    def files(self):
+        pass
+    
+    
+    @abstractproperty
+    def path(self):
+        pass
+    
+    
+    def process(self):
+        with BackupFile(self.name) as backup_file:
+            path = self.path
+            self.logger.debug('Backup: %s', path)
+            
+            for file in self.files:
+                backup_file.add(path.child(file), file)
+
+
+class Winamp (BackupTask):
+    @property
+    def files(self):
+        return [self.name + ext for ext in ('.bm', '.bm8', '.ini')]
+    
+    
+    @property
+    def name(self):
+        return 'Winamp'
+    
+    
+    @property
+    def path(self):
+        return automate.util.Path.settings().child(self.name)
