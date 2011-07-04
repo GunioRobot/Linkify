62da1d51bab7522df0108879a235636aae2c09b2
Marcio Faustino <marciof@home>
2011-07-04 23:47:00 +0200

    Added initial version of a browser widget to create HTML links from plain text URL's.

---
+++ automate/Widgets/Linkify/includes/index.js
@@ -0,0 +1,36 @@
+function loadScript(url, onLoad) {
+    var script = document.createElement('script');
+    
+    script.src = url;
+    script.onload = onLoad;
+    
+    document.body.appendChild(script);
+}
+
+
+function loadScripts(urls, onLoad) {
+    var url = urls.shift();
+    
+    loadScript(url, (urls.length == 0) ? onLoad : function () {
+        loadScripts(urls, onLoad);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var libraries = [
+        'http://code.jquery.com/jquery.js',
+        'https://raw.github.com/maranomynet/linkify/master/1.0/jquery.linkify-1.0.js',
+    ];
+    
+    loadScripts(libraries, function () {
+        window.jQuery('body').linkify({
+            handleLinks: function (links) {
+                links.css({
+                    color: '#0082E0',
+                    textDecoration: 'underline'
+                });
+            }
+        });
+    });
+}, false);
