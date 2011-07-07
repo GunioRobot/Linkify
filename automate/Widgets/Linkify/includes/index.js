aa5f3204a54572153abadca02bfb96ee8d9cbd0e
Marcio Faustino <marciof@home>
2011-07-07 21:34:59 +0200

    Replaced e-mail handler and basic HTTP handler with a more relaxed URL handler.

--- automate/Widgets/Linkify/includes/index.js
+++ automate/Widgets/Linkify/includes/index.js
@@ -19,19 +19,12 @@ function Handler() {
 }
 
 
-function HttpHandler() {
-    this.pattern = /\bhttps?:\/\/\w+\.\w+\.\w+/i;
+function UrlHandler() {
+    /** @see http://daringfireball.net/2010/07/improved_regex_for_matching_urls */
+    this.pattern = /\b(?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\))+(?:\(?:(?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])/i;
 };
 
-HttpHandler.prototype = new Handler();
-
-
-function MailtoHandler() {
-    /** @see http://docs.jquery.com/Plugins/Validation */
-    this.pattern = /\bmailto:\s*(?:(?:(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(?:\.(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|(?:(?:\x22)(?:(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:\\(?:[\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:\x22)))@(?:(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))+/i;
-};
-
-MailtoHandler.prototype = new Handler();
+UrlHandler.prototype = new Handler();
 
 
 function addLinksToElement(element, options) {
@@ -126,7 +119,7 @@ function log(/* ... */) {
 document.addEventListener('readystatechange', function() {
     var options = {
         excludedTags: /^(a|applet|area|button|embed|frame|frameset|iframe|img|map|object|option|param|script|select|style|textarea)$/i,
-        handlers: [new HttpHandler(), new MailtoHandler()]
+        handlers: [new UrlHandler()]
     };
     
     log('Update:', document.body);
