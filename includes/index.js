74f01e8cdafad7ae97a7d16c060397466d8c4ea2
Marcio Faustino <marciof@home>
2011-07-09 20:32:15 +0200

    Use String.replace to allow capturing groups in patterns.

--- includes/index.js
+++ includes/index.js
@@ -62,7 +62,7 @@ function UrlHandler() {
             + '|'
             + this.domainPattern.source
             + ')(?:[^\\s()<>"]+|\\((?:[^\\s()<>"]+|(?:\\([^\\s()<>"]+\\)))*\\))+(?:\\(?:(?:[^\\s()<>"]+|(?:\\([^\\s()<>"]+\\)))*\\)|[^\\s()<>"\\[\\]{};:\'`.,!?«»“”‘’])',
-        'i');
+        'gi');
 };
 
 UrlHandler.subClass(Handler, {
@@ -85,7 +85,7 @@ function EmailAddressHandler() {
     /**
      * @see http://docs.jquery.com/Plugins/Validation/Methods/email
      */
-    this.pattern = /\b(?:(?:(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(?:\.(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|(?:(?:\x22)(?:(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:\\(?:[\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:\x22)))@(?:(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))+/i;
+    this.pattern = /\b(?:(?:(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(?:\.(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|(?:(?:\x22)(?:(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:\\(?:[\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:\x22)))@(?:(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))+/gi;
 };
 
 EmailAddressHandler.subClass(Handler, {
@@ -154,25 +154,27 @@ function splitTextNode(textNode, options) {
 
 
 function splitTextNodeByHandler(textNode, handler) {
+    var separator = '\0';
+    var matches = [];
     var nodes = [];
-    var textParts = textNode.nodeValue.split(
-        RegExp('(' + handler.pattern.source + ')'));
     
-    if ((textParts.length == 1) && (textParts[0] === textNode.nodeValue)) {
+    var textParts = textNode.nodeValue.replace(handler.pattern, function () {
+        matches.push(handler.replacement.apply(handler, arguments));
+        return separator;
+    }).split(separator);
+    
+    if (matches.length == 0) {
         return nodes;
     }
     
-    for (var i = 0; i < textParts.length; ++i) {
-        var text = textParts[i];
+    while (matches.length > 0) {
+        var text = textParts.shift();
         
         if (text.length > 0) {
-            if ((i % 2) == 0) {
-                nodes.push(document.createTextNode(text));
-            }
-            else {
-                nodes.push(handler.replacement(text));
-            }
+            nodes.push(document.createTextNode(text));
         }
+        
+        nodes.push(matches.shift());
     }
     
     return nodes;
