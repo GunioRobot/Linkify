5ed47cb28102d3d40f7c275c7786f733240fe05a
Marcio Faustino <marciof@home>
2011-07-10 12:13:47 +0200

    Added documentation.

--- includes/index.js
+++ includes/index.js
@@ -1,3 +1,11 @@
+/**
+ * @fileOverview Adds event listeners for page loads and DOM updates.
+ */
+
+
+/**
+ * @private
+ */
 Array.from = function(object) {
     return Array.prototype.map.call(object, function (element) {
         return element;
@@ -5,6 +13,9 @@ Array.from = function(object) {
 };
 
 
+/**
+ * @private
+ */
 Function.prototype.subClass = function(BaseClass, methods) {
     // Unless an intermediate class is used, the prototype of the sub class
     // would also be the same of the base class. If so, any changes in the
@@ -27,10 +38,27 @@ Function.prototype.subClass = function(BaseClass, methods) {
 };
 
 
+/**
+ * @class Text handler abstract base class.
+ */
 function Handler() {
+    /**
+     * Search pattern.
+     * 
+     * @type RegExp
+     * @default null
+     */
     this.pattern = null;
 }
 
+
+/**
+ * Creates an HTML anchor element as the replacement node.
+ * 
+ * @param {String} href link
+ * @param {String} [caption=href] caption text
+ * @returns {HTMLAnchorElement}
+ */
 Handler.prototype.replacement = function (href, caption) {
     var anchor = document.createElement('a');
     
@@ -41,19 +69,30 @@ Handler.prototype.replacement = function (href, caption) {
 };
 
 
+/**
+ * @class URL style text handler.
+ * @augments Handler
+ */
 function UrlHandler() {
+    /**
+     * Whether or not URL's without a protocol should be modified to use HTTP.
+     * 
+     * @default true
+     * @type Boolean
+     */
     this.forceAbsoluteUrls = true;
     
-    this.ip4Address = /(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})/;
-    
+    this.ipv4Address = /(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})/;
+    this.protocolPattern = /[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%](?![\w-]*\s*=\s*"))/i;
     this.domainPattern = RegExp(
         '(?:www\\d{0,3}\\.)|(?:[a-z0-9.\\-]+\\.[a-z]{2,4}/)|'
-            + this.ip4Address.source,
+            + this.ipv4Address.source,
         'i');
     
-    this.protocolPattern = /[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%](?![\w-]*\s*=\s*"))/i;
-    
     /**
+     * URL search pattern.
+     * 
+     * @type RegExp
      * @see http://daringfireball.net/2010/07/improved_regex_for_matching_urls
      */
     this.pattern = RegExp(
@@ -65,7 +104,17 @@ function UrlHandler() {
         'gi');
 };
 
+
+/**
+ * @lends UrlHandler#
+ */
 UrlHandler.subClass(Handler, {
+    /**
+     * Creates an HTML anchor element for an URL.
+     * 
+     * @param {String} url URL found
+     * @returns {HTMLAnchorElement}
+     */
     replacement: function (url) {
         var caption = url;
         
@@ -81,20 +130,45 @@ UrlHandler.subClass(Handler, {
 });
 
 
+/**
+ * @class E-mail address text handler.
+ * @augments Handler
+ */
 function EmailAddressHandler() {
     /**
+     * E-mail address search pattern.
+     * 
+     * @type RegExp
      * @see http://docs.jquery.com/Plugins/Validation/Methods/email
      */
     this.pattern = /\b(?:(?:(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(?:\.(?:[a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|(?:(?:\x22)(?:(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:\\(?:[\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:\x22)))@(?:(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(?:(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])(?:[a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*(?:[a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))+/gi;
 };
 
+
+/**
+ * @lends EmailAddressHandler#
+ */
 EmailAddressHandler.subClass(Handler, {
+    /**
+     * Creates an HTML anchor element for an e-mail address.
+     * 
+     * @param {String} email e-mail address found
+     * @returns {HTMLAnchorElement}
+     */
     replacement: function (email) {
         return this.baseClass.replacement.call(this, 'mailto:' + email, email);
     }
 });
 
 
+/**
+ * Recursively applies handlers to text nodes. 
+ * 
+ * @param {Element} element root element where to start the conversion
+ * @param {Object} options
+ * @param {RegExp} options.excludedTags HTML elements to be excluded
+ * @param {Handler[]} options.handlers handlers to be applied 
+ */
 function addLinksToElement(element, options) {
     if (options.excludedTags.test(element.tagName)) {
         return;
@@ -124,6 +198,9 @@ function addLinksToElement(element, options) {
 }
 
 
+/**
+ * @private
+ */
 function splitTextNode(textNode, options) {
     var nodes = [textNode];
     
@@ -153,6 +230,9 @@ function splitTextNode(textNode, options) {
 }
 
 
+/**
+ * @private
+ */
 function splitTextNodeByHandler(textNode, handler) {
     var separator = '\0';
     var matches = [];
@@ -187,6 +267,9 @@ function splitTextNodeByHandler(textNode, handler) {
 }
 
 
+/**
+ * @private
+ */
 function log(/* ... */) {
     opera.postError(Array.from(arguments).join(' '));
 }
